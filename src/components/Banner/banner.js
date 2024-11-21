f3c18b526f5a7e09f796a38ff31987921753bb0d
William Monteiro <williamfm64@gmail.com>
2024-11-20 22:04:49 -0300

    Primeiro commit, projeto iniciado

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,11 @@
+import "./banner.css";
+
+function Banner (){
+    return(
+        <header className="banner">
+            <img src="/images/banner.png" alt="O banner do site"/>
+        </header>
+    )
+}
+
+export default Banner;
